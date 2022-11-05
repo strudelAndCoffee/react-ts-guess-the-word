77edd0cd565fae7715a7864b6f0f141a5b7228d9
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-11-05 17:51:26 -0500

    create Word component

--- src/Word.tsx
+++ src/Word.tsx
@@ -1,1 +1,20 @@
-export function Word() {}
+export function Word() {
+    const word = "test"
+    const guessedLetters = ["t"]
+    return <div style={{
+        display: 'flex',
+        gap: '.25em',
+        fontSize: '6rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace'
+    }}>
+        {word.split('').map((letter, index) => (
+            <span style={{ borderBottom: '.1em solid black' }}>
+                <span
+                    style={{ visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden' }} key={index}
+                >{letter}</span>
+            </span>
+        ))}
+    </div>
+}
